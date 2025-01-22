a3755072e8080e661bbe885faff6cac3ae2e4a24
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2025-01-22 17:19:29 +0200

    WIP

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function ($) {
-    "use strict";
+    'use strict';
 
     // Spinner
     var spinner = function () {
@@ -10,11 +10,9 @@
         }, 1);
     };
     spinner();
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-
 
     // Sticky Navbar
     $(window).scroll(function () {
@@ -24,36 +22,34 @@
             $('.navbar').removeClass('sticky-top shadow-sm');
         }
     });
-    
-    
+
     // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-    
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
+    const $dropdown = $('.dropdown');
+    const $dropdownToggle = $('.dropdown-toggle');
+    const $dropdownMenu = $('.dropdown-menu');
+    const showClass = 'show';
+
+    $(window).on('load resize', function () {
+        if (this.matchMedia('(min-width: 992px)').matches) {
             $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
+                function () {
+                    const $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr('aria-expanded', 'true');
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function () {
+                    const $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr('aria-expanded', 'false');
+                    $this.find($dropdownMenu).removeClass(showClass);
+                },
             );
         } else {
-            $dropdown.off("mouseenter mouseleave");
+            $dropdown.off('mouseenter mouseleave');
         }
     });
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -63,57 +59,55 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-
 
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
-        time: 2000
+        time: 2000,
     });
-
 
     // Modal Video
     $(document).ready(function () {
         var $videoSrc;
         $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
+            $videoSrc = $(this).data('src');
         });
         console.log($videoSrc);
 
         $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
+            $('#video').attr(
+                'src',
+                $videoSrc + '?autoplay=1&amp;modestbranding=1&amp;showinfo=0',
+            );
+        });
 
         $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
+            $('#video').attr('src', $videoSrc);
+        });
     });
 
-
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
+    $('.testimonial-carousel').owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
         center: true,
         margin: 24,
         dots: true,
         loop: true,
-        nav : false,
+        nav: false,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1,
             },
-            768:{
-                items:2
+            768: {
+                items: 2,
             },
-            992:{
-                items:3
-            }
-        }
+            992: {
+                items: 2,
+            },
+        },
     });
-    
 })(jQuery);
-
